252240710233525e339c6a305196c70fe8909c0a
shreyansh225 <54203870+shreyansh225@users.noreply.github.com>
2020-03-03 02:08:59 +0530

    Update List_Comprehensions.py

--- List_Comprehensions.py
+++ List_Comprehensions.py
@@ -14,3 +14,14 @@ for i in range(x+1):
                 result[p] = [ i, j, k]
                 p+=1 
 print(result)                
+
+#=================OR======================OR===================OR======================OR=================
+#=================OR======================OR===================OR======================OR=================
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if((i+j+k) != n)])
