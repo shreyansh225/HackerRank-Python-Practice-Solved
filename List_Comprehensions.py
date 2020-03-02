7d6bd1128d3003de62dd4630ebc52c622701148f
shreyansh225 <54203870+shreyansh225@users.noreply.github.com>
2020-03-03 02:04:14 +0530

    Create List_Comprehensions.py

---
+++ List_Comprehensions.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+result= []
+p = 0 
+for i in range(x+1):
+    for j in range(y+1): 
+        for k in range(z+1):
+            if i+j+k != n: 
+                result.append([])
+                result[p] = [ i, j, k]
+                p+=1 
+print(result)                
