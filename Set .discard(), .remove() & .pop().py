717c4dd9d2a5f561a57eeee1f9b33f7c94021af6
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-03-09 01:39:16 +0530

    Update Set .discard(), .remove() & .pop().py

--- Set .discard(), .remove() & .pop().py
+++ Set .discard(), .remove() & .pop().py
@@ -5,3 +5,13 @@ for _ in range(int(input())):
     x = list(input().split())
     eval('s.{0}({1})'.format(*x+['']))
 print(sum(s))
+
+
+#_____________________________OR________________________________________
+
+n = int(input())
+s = set(map(int, input().split())) 
+for i in range(int(input())):
+    eval('s.{0}({1})'.format(*input().split()+['']))
+
+print(sum(s))
