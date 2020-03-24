96b44266170bdf00ea57e943fe37b5659a1b5a79
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-03-24 14:08:06 +0530

    Update Find a string.py

--- Find a string.py
+++ Find a string.py
@@ -1,5 +1,9 @@
 def count_substring(string, sub_string):
-    return(sum([1 for i in range(0, len(string) - len(sub_string) + 1) if (string[i:(len(sub_string)+i)] == sub_string)]))
+    count = 0
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i+len(sub_string)] == sub_string:
+            count += 1
+    return count
 
 if __name__ == '__main__':
     string = input().strip()
@@ -8,11 +12,8 @@ if __name__ == '__main__':
     count = count_substring(string, sub_string)
     print(count)
     
-#.......................OR....................
+#.......................OR.................................................................................
     
-    def count_substring(string, sub_string):
-    count = 0
-    for i in range(len(string) - len(sub_string) + 1):
-        if string[i:i+len(sub_string)] == sub_string:
-            count += 1
-    return count
+def count_substring(string, sub_string):
+    return(sum([1 for i in range(0, len(string) - len(sub_string) + 1) if (string[i:(len(sub_string)+i)] == sub_string)]))
+    
