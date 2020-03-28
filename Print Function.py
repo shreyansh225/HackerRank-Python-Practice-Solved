b686fc836aa927b691b06e1e9fa1d066dd9ea52c
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-03-28 17:14:10 +0530

    Update Print Function.py

--- Print Function.py
+++ Print Function.py
@@ -1,4 +1,4 @@
-if __name__= '__main__'
+if __name__= '__main__':
   n=int(input())
 
 for i in range(1,n+1):
@@ -6,5 +6,5 @@ for i in range(1,n+1):
   
   ######################################################OROROROROR######################################
   
-  for i in range(n):
+for i in range(n):
   print(i+1,end='')
