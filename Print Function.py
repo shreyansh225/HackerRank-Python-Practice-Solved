08ee8cfd5b84589bfb85bc2eb20a8167e04091ee
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-03-28 17:04:44 +0530

    Create Print Function.py

---
+++ Print Function.py
@@ -0,0 +1,10 @@
+if __name__= '__main__'
+  n=int(input())
+
+for i in range(1,n+1):
+  print(i,end='')
+  
+  ######################################################OROROROROR######################################
+  
+  for i in range(n):
+  print(i+1,end='')
