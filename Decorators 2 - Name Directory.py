e9a26ec53fcf54bf7ce6e51f3aebd5fc7b8e8c2e
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-03-31 21:57:34 +0530

    Create Decorators 2 - Name Directory.py

---
+++ Decorators 2 - Name Directory.py
@@ -0,0 +1,28 @@
+import operator
+
+def person_lister(f):
+    def inner(people):
+        # complete the function
+        return map(f, sorted(people, key=lambda x: int(x[2])))        
+    return inner
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
+    
+
+"""
+Input (stdin)
+  3
+  Mike Thomson 20 M
+  Robert Bustle 32 M
+  Andria Bustle 30 F
+Expected Output
+  Mr. Mike Thomson
+  Ms. Andria Bustle
+  Mr. Robert Bustle
+"""
